68cdb68fa36946ae46c5fdbf03e8e3cf45f0fcfb
ilss <375259904@qq.com>
2018-12-12 10:03:10 +0800

    优化后退逻辑

--- src/lib/pathBreadcrumb.vue
+++ src/lib/pathBreadcrumb.vue
@@ -16,7 +16,7 @@
     <span v-for="(item,index) of pathData"
           :key="index">
       <a href="javascript:void(0)"
-         v-if="index===0 && index!==pathData.length-1"     
+         v-if="index===0 && index!==pathData.length-1"
          @click="gotoPath(item)">{{allFileLabel}}</a>
       <i v-else-if="index === 0 && index===pathData.length-1">{{allFileLabel}}</i>
       <a href="javascript:void(0)"
@@ -31,29 +31,29 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       myPath: null,
       historyEvent: null,
       pathData: null
       // sessionKey: 'seduFileManagementPathData'
-    }
+    };
   },
   props: {
     // 自定义行内样式
     myStyle: {
       type: String,
-      default: ''
+      default: ""
     },
     // 自定义class
     cname: {
       type: String,
-      default: 'components-path-breadcrumb'
+      default: "components-path-breadcrumb"
     },
     // 匹配文件夹的key
     matchKeyName: {
       type: String,
-      default: 'key'
+      default: "key"
     },
     // 点击面包屑链接 的回调函数
     callBack: {
@@ -61,19 +61,19 @@ export default {
       required: true
     },
     // 支持自定义文字，方便国际化
-    backLabel:{
+    backLabel: {
       type: String,
-      default:"返回上一级" 
+      default: "返回上一级"
     },
     // 支持自定义文字，方便国际化
-    allFileLabel:{
+    allFileLabel: {
       type: String,
-      default:"全部文件" 
-    },
+      default: "全部文件"
+    }
   },
-  created () {
-    this.myPath = true
-    this.pathData = []
+  created() {
+    this.myPath = true;
+    this.pathData = [];
     // if (this.historyEvent === null) {
     //   this.historyEvent = window.addEventListener('popstate', (event) => {
     //     console.log(String(window.history.state.title))
@@ -84,49 +84,48 @@ export default {
     //   })
     // }
   },
-  mounted () {
-  },
-  beforeDestroy () {
+  mounted() {},
+  beforeDestroy() {
     // window.removeEventListener('popstate', () => console.log('清除了监听'))
-    this.myPath = null
-    this.historyEvent = null
-    this.pathData = null
+    this.myPath = null;
+    this.historyEvent = null;
+    this.pathData = null;
   },
-  watch: {
-  },
+  watch: {},
   methods: {
     // 存入点击的路径对象
-    addPath (targetPath) {
-      this.pathData.push(targetPath)
+    addPath(targetPath) {
+      this.pathData.push(targetPath);
       if (this.pathData.length === 1) {
-        this.pathData[0]['name'] = this.allFileLabel
+        this.pathData[0]["name"] = this.allFileLabel;
       }
     },
-    gotoPath (path) {
+    gotoPath(path) {
       let _index = this.pathData.findIndex((value, index) => {
         if (value[this.matchKeyName] === path[this.matchKeyName]) {
-          return String(index)
+          return String(index);
         }
-      })
-      const len = this.pathData.length
-      Number(_index)
-      this.pathData.splice(_index === 0 ? 1 : _index + 1, len)
-      this.selectPath(path)
+      });
+      const len = this.pathData.length;
+      Number(_index);
+      this.pathData.splice(_index === 0 ? 1 : _index + 1, len);
+      this.selectPath(path);
     },
-    selectPath (path) {
-      this.callBack(path)
+    selectPath(path) {
+      this.callBack(path);
     },
-    goBack () {
-      const path = this.pathData.pop()
-      this.selectPath(path)
+    goBack() {
+      let len = this.pathData.length;
+      const path = this.pathData[len - 2];
+      this.pathData.pop();
+      this.selectPath(path);
     },
-    updatePathData (targetPath) {
+    updatePathData(targetPath) {
       // const state = {
       //   title: 'fileManagement',
       //   url: ''
       // }
       // window.history.pushState(state, 'title')
-
       // this.saveData(this.pathData)
     }
     // saveSessionData (data) {
@@ -134,5 +133,5 @@ export default {
     // },
     // getSessionData: () => sessionStorage.getItem(this.sessionKey)
   }
-}
+};
 </script>
